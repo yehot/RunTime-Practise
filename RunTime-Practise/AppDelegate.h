d922a87ff299ec3d005bdbd1ef53e622ba733164
yehot <yehot2013@gmail.com>
2018-05-25 16:30:48 +0800

    first init

---
+++ RunTime-Practise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RunTime-Practise
+//
+//  Created by yehot on 2018/5/25.
+//  Copyright © 2018年 Xin Hua Zhi Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RunTime-Practise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTime-Practise
+//
+//  Created by yehot on 2018/5/25.
+//  Copyright © 2018年 Xin Hua Zhi Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
